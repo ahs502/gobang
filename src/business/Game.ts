5d8bd4b6c892cd115172d90bbc4af5580ea66877
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-11-08 21:44:41 +0330

    Fix winning check algorithm

--- src/business/Game.ts
+++ src/business/Game.ts
@@ -21,37 +21,35 @@ export default class Game {
    * @param roomPosition The position of the target room.
    */
   set(playerType: PlayerType, { row, column }: RoomPosition): boolean {
-    if (this.status === 'FINISHED' || this.status === 'WITHDRAWAL') throw new Error('The game is already over!');
-    if (row < 0 || row >= this.size || column < 0 || column >= this.size) throw new Error('Out of board.');
-    if (this.state[row][column] !== 'NONE') throw new Error('This room is set before.');
+    const { size, state, status } = this;
 
-    this.state[row][column] = playerType;
+    if (status === 'FINISHED' || status === 'WITHDRAWAL') throw new Error('The game is already over!');
+    if (row < 0 || row >= size || column < 0 || column >= size) throw new Error('Out of board.');
+    if (state[row][column] !== 'NONE') throw new Error('This room is set before.');
+
+    state[row][column] = playerType;
 
     // Check whether the player wins or not:
-    const fromTop = row >= 4;
-    const fromBottom = row <= this.size - 5;
-    const fromLeft = column >= 4;
-    const fromRight = column <= this.size - 5;
-
-    const won =
-      (fromTop && check(-1, 0)) ||
-      (fromBottom && check(1, 0)) ||
-      (fromLeft && check(0, -1)) ||
-      (fromRight && check(0, 1)) ||
-      (fromTop && fromLeft && check(-1, -1)) ||
-      (fromTop && fromRight && check(-1, 1)) ||
-      (fromBottom && fromLeft && check(1, -1)) ||
-      (fromBottom && fromRight && check(1, 1));
+    const won = check(1, 0) || check(0, 1) || check(1, 1);
 
     // Update the status of the game:
-    this.status = won ? 'FINISHED' : this.status + 1 === this.size ** 2 ? 'WITHDRAWAL' : this.status + 1;
+    this.status = won ? 'FINISHED' : status + 1 === size ** 2 ? 'WITHDRAWAL' : status + 1;
 
     return won;
 
-    const state = this.state;
-    function check(rowStep: -1 | 0 | 1, columnStep: -1 | 0 | 1): boolean {
-      for (let i = 0; i < 5; ++i) {
-        if (state[row + i * rowStep][column + i * columnStep] !== playerType) return false;
+    function check(rowStep: 0 | 1, columnStep: 0 | 1): boolean {
+      let rowBase = row;
+      let columnBase = column;
+      while (
+        rowBase >= rowStep &&
+        columnBase >= columnStep &&
+        state[(rowBase -= rowStep)][(columnBase -= columnStep)] === playerType
+      ) {}
+      for (let i = 1; i <= 5; ++i) {
+        const rowCheck = rowBase + i * rowStep;
+        const columnCheck = columnBase + i * columnStep;
+        if (rowCheck >= size || columnCheck >= size) return false;
+        if (state[rowCheck][columnCheck] !== playerType) return false;
       }
       return true;
     }
