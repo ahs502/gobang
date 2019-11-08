44a90425e47d2a29b62fc8a60ea4d96bd366158b
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-11-08 21:59:05 +0330

    Add proper comments on Player.ts

--- src/business/Player.ts
+++ src/business/Player.ts
@@ -5,11 +5,17 @@ import RoomPosition from 'src/types/RoomPosition';
 export default abstract class Player {
   constructor(public name: string) {}
 
+  /** Returns whether this player represents a human activity or a bot? */
   abstract get actor(): 'HUMAN' | 'BOT';
 
   toString(): string {
     return `${this.name}${this.actor === 'BOT' ? ' [BOT]' : ''}`;
   }
 
+  /**
+   * Returns a promise to resolve the decided room position for the next move.
+   * @param game Which game to play right now?
+   * @param as The role of this player in the specified game.
+   */
   abstract async play(game: Game, as: PlayerType): Promise<RoomPosition>;
 }
